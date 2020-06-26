4747fb4860e71f777d8ae5e2fe076278d22f9627
Eduardo Eidelwein Berlitz <eduardo.berlitz@sap.com>
2020-06-26 18:51:51 -0300

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
-git branch -f azure
-git checkout azure
-echo 'Created and Checked out azure branch'
 
-git reset --hard origin/master
+target=$1
+if [ -z "$1" ]
+then
+      echo "\$target is empty"
+      exit 1
+fi
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+echo "Publishing '$branch' branch to '$target'"
+while true; do
+    read -p 'Do you wish to proceed (y/n)? ' yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+git branch -f $target
+git checkout $target
+echo 'Created and Checked out $target branch'
+
+git reset --hard origin/$branch
 
 npm i
 npm run build:prod
@@ -14,8 +33,8 @@ echo 'Tracking files'
 git add -A .
 git add -A dist -f
 echo 'Commiting files'
-git commit -a -m 'azure update'
-echo 'Pushing files into azure branch'
-git push origin azure --force
+git commit -a -m '$target update'
+echo 'Pushing files into $target branch'
+git push origin $target --force
 git checkout -
 npm i
